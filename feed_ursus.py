8cfb8178bbc20cde56cbf323c9b91b9de56346fc
Andrew Wallace <andrewwallace@library.ucla.edu>
2019-11-28 15:56:40 -0800

    set up .travis.yml

--- feed_ursus.py
+++ feed_ursus.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
+import collections
 import urllib.parse
 import pprint
-from typing import Dict, List, Optional
+import typing
 
 import click
-import pandas
-from pysolr import Solr
+import pandas  # type: ignore
+from pysolr import Solr  # type: ignore
 
 import mapper
 
@@ -13,7 +14,7 @@ import mapper
 @click.command()
 @click.argument('filename')
 @click.option('--solr_url', default=None, help='URL of a solr instance, e.g. http://localhost:6983/solr/californica')
-def load_csv(filename: str, solr_url: Optional[str]):
+def load_csv(filename: str, solr_url: typing.Optional[str]):
   solr_client = Solr(solr_url, always_commit=True) if solr_url else None
 
   df = pandas.read_csv(filename)
@@ -42,7 +43,7 @@ def load_csv(filename: str, solr_url: Optional[str]):
 def map_field_name(field_name: str) -> str:
   return mapper.FIELDS[field_name]
 
-def map_field_value(field_name: str, value: str) -> List[str]:
+def map_field_value(field_name: str, value: str) -> typing.List[str]:
   function_name = 'map_' + map_field_name(field_name)
   if hasattr(mapper, function_name):
     return getattr(mapper, function_name)(value)
@@ -51,10 +52,11 @@ def map_field_value(field_name: str, value: str) -> List[str]:
   else:
     return value.split('|~|')
 
-def map_record(record) -> Dict:
-  new_record = {map_field_name(key): map_field_value(key, value) 
-            for key, value in record.items()
-            if key in mapper.FIELDS.keys() and value}
+def map_record(record) -> typing.Dict[str, typing.Any]:
+  new_record: typing.Dict[str, typing.Any] = collections.defaultdict(list)
+  new_record.update({map_field_name(key): map_field_value(key, value) 
+                     for key, value in record.items()
+                     if key in mapper.FIELDS.keys() and value})
 
   new_record['id'] = new_record['ark_ssi']
 
@@ -67,14 +69,14 @@ def map_record(record) -> Dict:
   new_record['iiif_manifest_url_ssi'] = f'https://iiif.library.ucla.edu/{urllib.parse.quote_plus(record["Item ARK"])}/manifest'
 
   # facet fields
-  new_record['genre_sim'] = new_record.get('genre_tesim')
-  new_record['human_readable_language_sim'] = new_record.get('language_tesim')
-  new_record['human_readable_resource_type_sim'] = new_record.get('resource_type_tesim')
-  new_record['location_sim'] = new_record.get('location_tesim')
-  new_record['member_of_collections_ssim'] = new_record.get('dlcs_collection_name_tesim')
-  new_record['named_subject_sim'] = new_record.get('named_subject_tesim')
-  new_record['subject_sim'] = new_record.get('subject_tesim')
-  new_record['year_isim'] = new_record.get('year_tesim')
+  new_record['genre_sim'].extend(new_record['genre_tesim'])
+  new_record['human_readable_language_sim'].extend(new_record['language_tesim'])
+  new_record['human_readable_resource_type_sim'].extend(new_record['resource_type_tesim'])
+  new_record['location_sim'].extend(new_record['location_tesim'])
+  new_record['member_of_collections_ssim'].extend(new_record['dlcs_collection_name_tesim'])
+  new_record['named_subject_sim'].extend(new_record['named_subject_tesim'])
+  new_record['subject_sim'].extend(new_record['subject_tesim'])
+  new_record['year_isim'].extend(new_record['year_tesim'])
 
   return new_record
 
