76fbd2f78bfeb596e9ddd9452940ddc67f3c2b95
Andrew Wallace <andrewwallace@library.ucla.edu>
2019-11-28 16:18:30 -0800

    auto-format with black

--- feed_ursus.py
+++ feed_ursus.py
@@ -12,73 +12,93 @@ import mapper
 
 
 @click.command()
-@click.argument('filename')
-@click.option('--solr_url', default=None, help='URL of a solr instance, e.g. http://localhost:6983/solr/californica')
+@click.argument("filename")
+@click.option(
+    "--solr_url",
+    default=None,
+    help="URL of a solr instance, e.g. http://localhost:6983/solr/californica",
+)
 def load_csv(filename: str, solr_url: typing.Optional[str]):
-  solr_client = Solr(solr_url, always_commit=True) if solr_url else None
+    solr_client = Solr(solr_url, always_commit=True) if solr_url else None
 
-  df = pandas.read_csv(filename)
-  df = df.where(df.notnull(), None)
+    df = pandas.read_csv(filename)
+    df = df.where(df.notnull(), None)
 
-  first_row = True
-  
-  if not solr_client:
-    print('[', end='')
+    first_row = True
 
-  for _, row in df.iterrows():
-    if first_row:
-      first_row = False
-    elif not solr_client:
-      print(', ')
+    if not solr_client:
+        print("[", end="")
 
-    mapped_record = map_record(row)
-    if solr_client:
-      solr_client.add([mapped_record])
-    else:
-      print(mapped_record, end='')
-  
-  if not solr_client:
-    print(']')
+    for _, row in df.iterrows():
+        if first_row:
+            first_row = False
+        elif not solr_client:
+            print(", ")
+
+        mapped_record = map_record(row)
+        if solr_client:
+            solr_client.add([mapped_record])
+        else:
+            print(mapped_record, end="")
+
+    if not solr_client:
+        print("]")
+
 
 def map_field_name(field_name: str) -> str:
-  return mapper.FIELDS[field_name]
+    return mapper.FIELDS[field_name]
+
 
 def map_field_value(field_name: str, value: str) -> typing.List[str]:
-  function_name = 'map_' + map_field_name(field_name)
-  if hasattr(mapper, function_name):
-    return getattr(mapper, function_name)(value)
-  elif value is None:
-    return []
-  else:
-    return value.split('|~|')
+    function_name = "map_" + map_field_name(field_name)
+    if hasattr(mapper, function_name):
+        return getattr(mapper, function_name)(value)
+    elif value is None:
+        return []
+    else:
+        return value.split("|~|")
+
 
 def map_record(record) -> typing.Dict[str, typing.Any]:
-  new_record: typing.Dict[str, typing.Any] = collections.defaultdict(list)
-  new_record.update({map_field_name(key): map_field_value(key, value) 
-                     for key, value in record.items()
-                     if key in mapper.FIELDS.keys() and value})
+    new_record: typing.Dict[str, typing.Any] = collections.defaultdict(list)
+    new_record.update(
+        {
+            map_field_name(key): map_field_value(key, value)
+            for key, value in record.items()
+            if key in mapper.FIELDS.keys() and value
+        }
+    )
 
-  new_record['id'] = new_record['ark_ssi']
+    new_record["id"] = new_record["ark_ssi"]
 
-  if record.get('IIIF Access URL'):
-    new_record['thumbnail_url_ss'] = record['IIIF Access URL'] + '/full/!200,200/0/default.jpg'
+    if record.get("IIIF Access URL"):
+        new_record["thumbnail_url_ss"] = (
+            record["IIIF Access URL"] + "/full/!200,200/0/default.jpg"
+        )
 
-  new_record['discover_access_group_ssim'] = ['public']
-  new_record['read_access_group_ssim'] = ['public']
-  new_record['download_access_person_ssim'] = ['public']
-  new_record['iiif_manifest_url_ssi'] = f'https://iiif.library.ucla.edu/{urllib.parse.quote_plus(record["Item ARK"])}/manifest'
+    new_record["discover_access_group_ssim"] = ["public"]
+    new_record["read_access_group_ssim"] = ["public"]
+    new_record["download_access_person_ssim"] = ["public"]
+    new_record[
+        "iiif_manifest_url_ssi"
+    ] = f'https://iiif.library.ucla.edu/{urllib.parse.quote_plus(record["Item ARK"])}/manifest'
 
-  # facet fields
-  new_record['genre_sim'].extend(new_record['genre_tesim'])
-  new_record['human_readable_language_sim'].extend(new_record['language_tesim'])
-  new_record['human_readable_resource_type_sim'].extend(new_record['resource_type_tesim'])
-  new_record['location_sim'].extend(new_record['location_tesim'])
-  new_record['member_of_collections_ssim'].extend(new_record['dlcs_collection_name_tesim'])
-  new_record['named_subject_sim'].extend(new_record['named_subject_tesim'])
-  new_record['subject_sim'].extend(new_record['subject_tesim'])
-  new_record['year_isim'].extend(new_record['year_tesim'])
+    # facet fields
+    new_record["genre_sim"].extend(new_record["genre_tesim"])
+    new_record["human_readable_language_sim"].extend(new_record["language_tesim"])
+    new_record["human_readable_resource_type_sim"].extend(
+        new_record["resource_type_tesim"]
+    )
+    new_record["location_sim"].extend(new_record["location_tesim"])
+    new_record["member_of_collections_ssim"].extend(
+        new_record["dlcs_collection_name_tesim"]
+    )
+    new_record["named_subject_sim"].extend(new_record["named_subject_tesim"])
+    new_record["subject_sim"].extend(new_record["subject_tesim"])
+    new_record["year_isim"].extend(new_record["year_tesim"])
 
-  return new_record
+    return new_record
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     load_csv()  # pylint: disable=no-value-for-parameter
--- test/test_feed_ursus.py
+++ test/test_feed_ursus.py
@@ -2,64 +2,101 @@ import pytest  # type: ignore
 
 import feed_ursus
 
+
 @pytest.mark.xfail()
 def test_load_csv():
-  raise NotImplementedError
+    raise NotImplementedError
+
 
 def test_map_field_name(monkeypatch):
-  monkeypatch.setitem(feed_ursus.mapper.FIELDS, 'Test DLCS Field', 'test_ursus_field_tesim')
-  assert feed_ursus.map_field_name('Test DLCS Field') == 'test_ursus_field_tesim'
+    monkeypatch.setitem(
+        feed_ursus.mapper.FIELDS, "Test DLCS Field", "test_ursus_field_tesim"
+    )
+    assert feed_ursus.map_field_name("Test DLCS Field") == "test_ursus_field_tesim"
+
 
 class TestMapFieldValue:
-  def test_parses_array(self, monkeypatch):
-    monkeypatch.setitem(feed_ursus.mapper.FIELDS, 'Test DLCS Field', 'test_ursus_field_tesim')
-    assert feed_ursus.map_field_value('Test DLCS Field', 'one|~|two|~|three') == ['one', 'two', 'three']
-  
-  def test_calls_function(self, monkeypatch):
-    monkeypatch.setitem(feed_ursus.mapper.FIELDS, 'Test DLCS Field', 'test_ursus_field_tesim')
-    monkeypatch.setattr(feed_ursus.mapper, 'map_test_ursus_field_tesim', lambda x: 'lkghsdh', raising=False)
-    feed_ursus.map_field_value('Test DLCS Field', 'one|~|two|~|three')
-    assert feed_ursus.mapper.map_test_ursus_field_tesim('abc') == 'lkghsdh'  # pylint: disable=no-member
+    def test_parses_array(self, monkeypatch):
+        monkeypatch.setitem(
+            feed_ursus.mapper.FIELDS, "Test DLCS Field", "test_ursus_field_tesim"
+        )
+        assert feed_ursus.map_field_value("Test DLCS Field", "one|~|two|~|three") == [
+            "one",
+            "two",
+            "three",
+        ]
+
+    def test_calls_function(self, monkeypatch):
+        monkeypatch.setitem(
+            feed_ursus.mapper.FIELDS, "Test DLCS Field", "test_ursus_field_tesim"
+        )
+        monkeypatch.setattr(
+            feed_ursus.mapper,
+            "map_test_ursus_field_tesim",
+            lambda x: "lkghsdh",
+            raising=False,
+        )
+        feed_ursus.map_field_value("Test DLCS Field", "one|~|two|~|three")
+        assert (
+            feed_ursus.mapper.map_test_ursus_field_tesim("abc") == "lkghsdh"
+        )  # pylint: disable=no-member
 
 
 class TestMapRecord:
-  def test_maps_record(self, monkeypatch):
-    monkeypatch.setitem(feed_ursus.mapper.FIELDS, 'Test DLCS Field', 'test_ursus_field_tesim')
-    result = feed_ursus.map_record({'Item ARK': 'ark:/123/abc', 'Test DLCS Field': 'lasigd|~|asdfg'})
-    assert result['id'] == 'ark:/123/abc'
-    assert result['ark_ssi'] == 'ark:/123/abc'
-    assert result['test_ursus_field_tesim'] == ['lasigd', 'asdfg']
+    def test_maps_record(self, monkeypatch):
+        monkeypatch.setitem(
+            feed_ursus.mapper.FIELDS, "Test DLCS Field", "test_ursus_field_tesim"
+        )
+        result = feed_ursus.map_record(
+            {"Item ARK": "ark:/123/abc", "Test DLCS Field": "lasigd|~|asdfg"}
+        )
+        assert result["id"] == "ark:/123/abc"
+        assert result["ark_ssi"] == "ark:/123/abc"
+        assert result["test_ursus_field_tesim"] == ["lasigd", "asdfg"]
 
-  def test_sets_id(self):
-    assert feed_ursus.map_record({'Item ARK': 'ark:/123/abc'})['id'] == 'ark:/123/abc'
+    def test_sets_id(self):
+        assert (
+            feed_ursus.map_record({"Item ARK": "ark:/123/abc"})["id"] == "ark:/123/abc"
+        )
 
-  def test_sets_thumbnail(self):
-    result = feed_ursus.map_record({'Item ARK': 'ark:/123/abc', 'IIIF Access URL': 'https://test.iiif.server/url'})
-    assert result['thumbnail_url_ss'] == 'https://test.iiif.server/url/full/!200,200/0/default.jpg'
+    def test_sets_thumbnail(self):
+        result = feed_ursus.map_record(
+            {
+                "Item ARK": "ark:/123/abc",
+                "IIIF Access URL": "https://test.iiif.server/url",
+            }
+        )
+        assert (
+            result["thumbnail_url_ss"]
+            == "https://test.iiif.server/url/full/!200,200/0/default.jpg"
+        )
 
-  def test_sets_access(self):
-    result = feed_ursus.map_record({'Item ARK': 'ark:/123/abc'})
-    assert result['discover_access_group_ssim'] == ['public']
-    assert result['read_access_group_ssim'] == ['public']
-    assert result['download_access_person_ssim'] == ['public']
+    def test_sets_access(self):
+        result = feed_ursus.map_record({"Item ARK": "ark:/123/abc"})
+        assert result["discover_access_group_ssim"] == ["public"]
+        assert result["read_access_group_ssim"] == ["public"]
+        assert result["download_access_person_ssim"] == ["public"]
 
-  def test_sets_iiif_manifest_url(self):
-    result = feed_ursus.map_record({'Item ARK': 'ark:/123/abc'})
-    assert result['iiif_manifest_url_ssi'] == 'https://iiif.library.ucla.edu/ark%3A%2F123%2Fabc/manifest'
+    def test_sets_iiif_manifest_url(self):
+        result = feed_ursus.map_record({"Item ARK": "ark:/123/abc"})
+        assert (
+            result["iiif_manifest_url_ssi"]
+            == "https://iiif.library.ucla.edu/ark%3A%2F123%2Fabc/manifest"
+        )
 
-  @pytest.mark.parametrize(
-    ['column_name', 'facet_field_name'],
-    [
-      ('Type.genre', 'genre_sim'),
-      ('Language', 'human_readable_language_sim'),
-      ('Type.typeOfResource', 'human_readable_resource_type_sim'),
-      ('Coverage.geographic', 'location_sim'),
-      ('Relation.isPartOf', 'member_of_collections_ssim'),
-      ('Name.subject', 'named_subject_sim'),
-      ('Subject', 'subject_sim'),
-    ]
-  )
-  def test_sets_facet_fields(self, column_name, facet_field_name):
-    value = 'value aksjg'
-    result = feed_ursus.map_record({'Item ARK': 'ark:/123/abc', column_name: value}) 
-    assert result[facet_field_name] == [value]
+    @pytest.mark.parametrize(
+        ["column_name", "facet_field_name"],
+        [
+            ("Type.genre", "genre_sim"),
+            ("Language", "human_readable_language_sim"),
+            ("Type.typeOfResource", "human_readable_resource_type_sim"),
+            ("Coverage.geographic", "location_sim"),
+            ("Relation.isPartOf", "member_of_collections_ssim"),
+            ("Name.subject", "named_subject_sim"),
+            ("Subject", "subject_sim"),
+        ],
+    )
+    def test_sets_facet_fields(self, column_name, facet_field_name):
+        value = "value aksjg"
+        result = feed_ursus.map_record({"Item ARK": "ark:/123/abc", column_name: value})
+        assert result[facet_field_name] == [value]
