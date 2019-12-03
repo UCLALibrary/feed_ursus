9af2c497039c6f79416b5bccfcc56ef98bee8148
Andrew Wallace <andrewwallace@library.ucla.edu>
2019-12-03 10:54:46 -0800

    Use collection row to set collection name for items

--- test/test_feed_ursus.py
+++ test/test_feed_ursus.py
@@ -54,13 +54,16 @@ class TestMapFieldValue:
 class TestMapRecord:
     """function map_record"""
 
+    COLLECTION_NAMES = {"ark:/123/collection": "Test Collection KGSL"}
+
     def test_maps_record(self, monkeypatch):
         """maps the record for Ursus"""
         monkeypatch.setitem(
             feed_ursus.mapper.FIELDS, "Test DLCS Field", "test_ursus_field_tesim"
         )
         result = feed_ursus.map_record(
-            {"Item ARK": "ark:/123/abc", "Test DLCS Field": "lasigd|~|asdfg"}
+            {"Item ARK": "ark:/123/abc", "Test DLCS Field": "lasigd|~|asdfg"},
+            self.COLLECTION_NAMES,
         )
         assert result["id"] == "ark:/123/abc"
         assert result["ark_ssi"] == "ark:/123/abc"
@@ -68,9 +71,10 @@ class TestMapRecord:
 
     def test_sets_id(self):
         """sets 'id' equal to 'Item ARK'/'ark_ssi'"""
-        assert (
-            feed_ursus.map_record({"Item ARK": "ark:/123/abc"})["id"] == "ark:/123/abc"
+        result = feed_ursus.map_record(
+            {"Item ARK": "ark:/123/abc"}, self.COLLECTION_NAMES,
         )
+        assert result["id"] == "ark:/123/abc"
 
     def test_sets_thumbnail(self):
         """sets a thumbnail URL"""
@@ -78,7 +82,8 @@ class TestMapRecord:
             {
                 "Item ARK": "ark:/123/abc",
                 "IIIF Access URL": "https://test.iiif.server/url",
-            }
+            },
+            self.COLLECTION_NAMES,
         )
         assert (
             result["thumbnail_url_ss"]
@@ -87,33 +92,49 @@ class TestMapRecord:
 
     def test_sets_access(self):
         """sets permissive values for blacklight-access-control"""
-        result = feed_ursus.map_record({"Item ARK": "ark:/123/abc"})
+        result = feed_ursus.map_record(
+            {"Item ARK": "ark:/123/abc"}, self.COLLECTION_NAMES,
+        )
         assert result["discover_access_group_ssim"] == ["public"]
         assert result["read_access_group_ssim"] == ["public"]
         assert result["download_access_person_ssim"] == ["public"]
 
     def test_sets_iiif_manifest_url(self):
         """sets a IIIF manifest URL based on the ARK"""
-        result = feed_ursus.map_record({"Item ARK": "ark:/123/abc"})
+        result = feed_ursus.map_record(
+            {"Item ARK": "ark:/123/abc"}, self.COLLECTION_NAMES,
+        )
         assert (
             result["iiif_manifest_url_ssi"]
             == "https://iiif.library.ucla.edu/ark%3A%2F123%2Fabc/manifest"
         )
 
+    def test_sets_collection(self):
+        """sets the collection name by using the collection row"""
+
+        result = feed_ursus.map_record(
+            {"Item ARK": "ark:/123/abc", "Parent ARK": "ark:/123/collection"},
+            self.COLLECTION_NAMES,
+        )
+        assert result["dlcs_collection_name_tesim"] == ["Test Collection KGSL"]
+        assert result["member_of_collections_ssim"] == ["Test Collection KGSL"]
+
     @pytest.mark.parametrize(
         ["column_name", "facet_field_name"],
         [
-            ("Type.genre", "genre_sim"),
-            ("Language", "human_readable_language_sim"),
-            ("Type.typeOfResource", "human_readable_resource_type_sim"),
             ("Coverage.geographic", "location_sim"),
-            ("Relation.isPartOf", "member_of_collections_ssim"),
+            ("Language", "human_readable_language_sim"),
             ("Name.subject", "named_subject_sim"),
+            ("Relation.isPartOf", "member_of_collections_ssim"),
             ("Subject", "subject_sim"),
+            ("Type.genre", "genre_sim"),
+            ("Type.typeOfResource", "human_readable_resource_type_sim"),
         ],
     )
     def test_sets_facet_fields(self, column_name, facet_field_name):
         """Copies *_tesim to *_sim fields for facets"""
         value = "value aksjg"
-        result = feed_ursus.map_record({"Item ARK": "ark:/123/abc", column_name: value})
+        result = feed_ursus.map_record(
+            {"Item ARK": "ark:/123/abc", column_name: value}, self.COLLECTION_NAMES,
+        )
         assert result[facet_field_name] == [value]
