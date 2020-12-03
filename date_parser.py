1b05de9ca6c1b3d8d9654dd035ab6a2f1b57faaa
pghorpade <pghorpade@library.ucla.edu>
2020-12-03 13:55:12 -0800

    pylint duplicate code refactor

--- date_parser.py
+++ date_parser.py
@@ -9,12 +9,10 @@ from dateutil import parser
 
 
 
-RANGE = re.compile(r"(.*)/(.*)")
-YEAR = re.compile(r"\b(\d\d\d\d|\d\d\d)\b")
+NORMALIZED_RANGE = re.compile(r"(.*)/(.*)")
 
-# pylint: disable=duplicate-code
-def get_dates(dates: typing.Any):
-    """Maps a list of 'normalized_date' strings to a sorted list of integer years.
+def get_dates(normalized_dates: typing.Any):
+    """Maps a list of 'normalized_date' strings to a sorted list of datetime.
 
     Args:
         dates: A list of strings containing dates in the 'normalized_date' format.
@@ -23,13 +21,13 @@ def get_dates(dates: typing.Any):
         A list of years extracted from "dates".
 
     """
-    if not isinstance(dates, typing.Iterable):
+    if not isinstance(normalized_dates, typing.Iterable):
         return []
     solr_dts = set()
-    for date in dates:
-        if not isinstance(date, str):
+    for normalized_date in normalized_dates:
+        if not isinstance(normalized_date, str):
             continue
-        match = RANGE.search(date)
+        match = NORMALIZED_RANGE.search(normalized_date)
         if match:
             start_str, end_str = match.groups()
             start = get_date(start_str)
@@ -37,7 +35,7 @@ def get_dates(dates: typing.Any):
             if start and end:
                 solr_dts.update({start, end})
         else:
-            solr_date = get_date(date)
+            solr_date = get_date(normalized_date)
             if solr_date:
                 solr_dts.add(solr_date)
     return sorted(solr_dts)
